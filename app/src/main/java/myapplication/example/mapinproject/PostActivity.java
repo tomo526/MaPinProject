7e2b3dc74f4053f141d733c4c371e056db61b8e4
nanashi <nanashi@gmail.com>
2019-12-19 10:06:12 +0900

    2019/12/19 コミットできなかった分

---
+++ app/src/main/java/myapplication/example/mapinproject/PostActivity.java
@@ -0,0 +1,42 @@
+package myapplication.example.mapinproject;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.ImageView;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PostActivity  extends AppCompatActivity {
+    private static final int RESULT_PICK_IMAGEFILE = 1000;
+    private ImageView imageView;
+
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.post);
+
+        findViewById(R.id.post_image_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("image/*");
+                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                startActivityForResult(intent, RESULT_PICK_IMAGEFILE);
+            }
+        });
+
+
+
+
+    }
+}
