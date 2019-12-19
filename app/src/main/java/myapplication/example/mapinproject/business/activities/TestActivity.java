c97afcbdd19bef263a095797e200667cc2cb9910
ogu22 <s.oguma22@gmail.com>
2019-12-19 09:33:41 +0900

    firebase周り

--- app/src/main/java/myapplication/example/mapinproject/business/activities/TestActivity.java
+++ app/src/main/java/myapplication/example/mapinproject/business/activities/TestActivity.java
@@ -1,7 +1,6 @@
 package myapplication.example.mapinproject.business.activities;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -10,61 +9,79 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.HashMap;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 import myapplication.example.mapinproject.R;
 import myapplication.example.mapinproject.data.entities.Test;
-import myapplication.example.mapinproject.data.entities.User;
+import myapplication.example.mapinproject.data.storage.TestStorage;
 
 public class TestActivity extends FragmentActivity implements View.OnClickListener {
 
-    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private FirebaseDatabase database;
     private EditText editText;
-    private TextView textView;
+    private EditText editTitle;
+    private EditText editSearch;
+    private TextView searchView;
+    private DatabaseReference ref;
+    private DatabaseReference testRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test);
         editText = findViewById(R.id.edit_text);
-        textView = findViewById(R.id.text_view);
+        editTitle = findViewById(R.id.edit_title);
+        editSearch = findViewById(R.id.edit_search);
+        searchView = findViewById(R.id.search_view);
         findViewById(R.id.test_create).setOnClickListener(this);
-        findViewById(R.id.test_read).setOnClickListener(this);
-        final FirebaseDatabase database = FirebaseDatabase.getInstance();
-    }
-
-    private void read() {
-        DatabaseReference ref = database.getReference("/test/testid");
-        ref.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                for (DataSnapshot data : dataSnapshot.getChildren()) {
-                    Test text = dataSnapshot.getValue(Test.class);
-                    textView.setText(text.getText());
-                }
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-                textView.setText("error");
-            }
-        });
-    }
-
-    private void create(String text) {
-        DatabaseReference ref = database.getReference("/test/testid");
-        Test test = new Test(text);
-        ref.setValue(test);
+        findViewById(R.id.test_search).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
+        TestStorage storage = new TestStorage();
         int i = v.getId();
         if (i == R.id.test_create) {
-            create(editText.getText().toString());
-        } else if (i == R.id.test_read) {
-            read();
+            storage.newCreate(editText.getText().toString(), editTitle.getText().toString());
+        } else if (i == R.id.test_search) {
+            search(editSearch.getText().toString());
         }
     }
+
+    public void search(String fstr) {
+        database = FirebaseDatabase.getInstance();
+        DatabaseReference testRef = database.getReference("test");
+        Query findRef = testRef.orderByChild("name").startAt(fstr).endAt(fstr + "\uf8ff");
+
+        ValueEventListener listener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                GenericTypeIndicator<HashMap<String, Test>> gt_indicator = new GenericTypeIndicator<HashMap<String, Test>>() {
+                };
+                HashMap<String, Test> values = snapshot.getValue(gt_indicator);
+                String res = "";
+                if (values != null) {
+                    for (Object p : values.values()) {
+                        res += p + "\n";
+                    }
+                } else {
+                    res = "no data.";
+                }
+                searchView.setText(res);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+            }
+        };
+        findRef.addListenerForSingleValueEvent(listener);
+    }
+
+
 }
--- app/src/main/java/myapplication/example/mapinproject/data/entities/Test.java
+++ app/src/main/java/myapplication/example/mapinproject/data/entities/Test.java
@@ -4,20 +4,30 @@ import com.google.firebase.database.IgnoreExtraProperties;
 
 @IgnoreExtraProperties
 public class Test {
-    public String text;
+    private String text;
+    private String name;
 
-    public Test(){
+    public Test() {
     }
 
-    public Test(String _text) {
-        text = _text;
+    public Test(String _text,String _name) {
+        this.text = _text;
+        this.name = _name;
+    }
+
+    @Override
+    public String toString() {
+        return "Test{" +
+                "name='" + name + '\'' +
+                ", text='" + text + '\'' +
+                '}';
     }
 
     public String getText() {
         return text;
     }
 
-    public void setText(String text) {
-        this.text = text;
+    public String getName() {
+        return name;
     }
 }
