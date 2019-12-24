842994e0d8671f147055e742111f8938d74f2dc6
ogu22 <s.oguma22@gmail.com>
2019-12-24 14:39:59 +0900

    Pin情報画面

--- app/src/main/java/myapplication/example/mapinproject/business/activities/Pin_infoActivity.java
+++ app/src/main/java/myapplication/example/mapinproject/business/activities/Pin_infoActivity.java
@@ -5,8 +5,8 @@ import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
-import myapplication.example.mapinproject.business.fragments.Pin_infoDialog;
 import myapplication.example.mapinproject.R;
+import myapplication.example.mapinproject.business.fragments.Pin_infoDialog;
 
 public class Pin_infoActivity extends AppCompatActivity {
 
@@ -14,19 +14,18 @@ public class Pin_infoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pin_info);
-
-//        // idがdialogButtonのButtonを取得
-//        Button dialogBtn = (Button) findViewById(R.id.dialogbutton3);
-//        // clickイベント追加
-//        dialogBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            // クリックしたらダイアログを表示する処理
-//            public void onClick(View v) {
-//                // ダイアログクラスをインスタンス化
-//                Pin_infoDialog dialog = new Pin_infoDialog();
-//                // 表示  getFagmentManager()は固定、sampleは識別タグ
-//                dialog.show(getFragmentManager(), "sample");
-//            }
-//        });
+        // idがdialogButtonのButtonを取得
+        Button dialogBtn = (Button) findViewById(R.id.dialogbutton3);
+        // clickイベント追加
+        dialogBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            // クリックしたらダイアログを表示する処理
+            public void onClick(View v) {
+                // ダイアログクラスをインスタンス化
+                Pin_infoDialog dialog = new Pin_infoDialog();
+                // 表示  getFagmentManager()は固定、sampleは識別タグ
+                dialog.show(getFragmentManager(), "sample");
+            }
+        });
     }
 }
