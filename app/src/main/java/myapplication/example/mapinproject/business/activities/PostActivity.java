ecabe73ccf57f5af7bb82a3818aa8ac86d59502d
ogu22 <s.oguma22@gmail.com>
2019-12-24 14:55:21 +0900

    PostActivityコメントアウト

--- app/src/main/java/myapplication/example/mapinproject/business/activities/PostActivity.java
+++ app/src/main/java/myapplication/example/mapinproject/business/activities/PostActivity.java
@@ -1,151 +1,103 @@
-package myapplication.example.mapinproject.business.activities;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
-import myapplication.example.mapinproject.R;
-import myapplication.example.mapinproject.data.storage.PostStorage;
-
-import myapplication.example.mapinproject.business.fragments.Post_Dialog_Confirmation;
-import static myapplication.example.mapinproject.business.activities.ReplyActivity.RESULT_PICK_IMAGEFILE;
-
-public class PostActivity extends AppCompatActivity implements View.OnClickListener {
-    private static final int RESULT_PICK_IMAGEFILE = 1000;
-    private ImageView imageView;
-
-    private FirebaseDatabase database;
-
-    private EditText locationText;
-    private EditText contentText;
-    private EditText categoryText;
-    private DatabaseReference ref;
-    private DatabaseReference testRef;
-
-public class PostActivity  extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.post);
-        final Button dialogBtn = (Button) findViewById(R.id.post_send_button);
-        final TextView textView = (TextView) findViewById(R.id.post_send_button);
-
-
-        locationText = findViewById(R.id.post_location_text);
-        contentText = findViewById(R.id.post_content_text);
-        categoryText = findViewById(R.id.post_category_text);
-        RatingBar rate = findViewById(R.id.ratingBar);
-        findViewById(R.id.post_back_button).setOnClickListener(this);
-        findViewById(R.id.post_image_button).setOnClickListener(this);
-        findViewById(R.id.post_send_button).setOnClickListener(this);
-//        rate.setOnRatingBarChangeListener(
-//                new RatingBar.OnRatingBarChangeListener(){
-//                    public void onRatingChanged(
-//                            RatingBar ratingBar,
-//                            float rating,
-//                            boolean fromUser){
-//                        Toast.makeText(
-//                                MainActivity.this,
-//                                String.format("現在の評価は%f", rating),
-//                                Toast.LENGTH_SHORT).show();
-//                    }
-//                }
-//        );
-        ImageButton imageButton = findViewById(R.id.post_image_button);
-
-//        imageButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if (flg) {
-//                    textImageButton.setText(R.string.tapped);
-//                    flg = false;
-//                } else {
-//                    textImageButton.setText(R.string.image_button);
-//                    flg = true;
-//                }
-//            }
-//        });
-    }
-
-    @Override
-    public void onClick(View v) {
-        PostStorage storage = new PostStorage();
-        int i = v.getId();
-        if (i == R.id.post_back_button) {
-            Pin_infoActivity();
-        } else if (i == R.id.post_image_button) {
-            ImageVive();
-        } else if (i == R.id.post_send_button) {
-            storage.newCreate(categoryText.getText().toString(), contentText.getText().toString(), locationText.getText().toString());
-        }
-    }
-
-    public void Pin_infoActivity() {
-        Intent intent = new Intent(PostActivity.this, Pin_infoActivity.class);
-        startActivity(intent);
-    }
-
-    public void ImageVive() {
-        findViewById(R.id.post_image_button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
-                intent.addCategory(Intent.CATEGORY_OPENABLE);
-                intent.setType("image/*");
-                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-                startActivityForResult(intent, RESULT_PICK_IMAGEFILE);
-            }
-        });
-
-        //送信時に行う
-        findViewById(R.id.post_send_button).setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                EditText text = (EditText)findViewById(R.id.post_content_text);
-                if(text.getText().toString().isEmpty()) {
-                    text.setError("文字を入力してください");
-                } else {
-                    dialogBtn.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                    public void onClick(View v) {
-                        // ダイアログクラスをインスタンス化
-                        Post_Dialog_Confirmation dialog = new Post_Dialog_Confirmation();
-                        // 表示  getFagmentManager()は固定、sampleは識別タグ
-                        dialog.show(getFragmentManager(), "sample");
-                    }
-                });
-
-
-
-
-
-
-
-
-
-
-                }
-            }
-        });
-    }
-
+//package myapplication.example.mapinproject.business.activities;
+//
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.view.View;
+//import android.widget.Button;
+//import android.widget.EditText;
+//import android.widget.ImageButton;
+//import android.widget.ImageView;
+//import android.widget.RatingBar;
+//import android.widget.TextView;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import com.google.firebase.database.DatabaseReference;
+//import com.google.firebase.database.FirebaseDatabase;
+//
+//import myapplication.example.mapinproject.R;
+//import myapplication.example.mapinproject.data.storage.PostStorage;
+//
+//import myapplication.example.mapinproject.business.fragments.Post_Dialog_Confirmation;
+//import static myapplication.example.mapinproject.business.activities.ReplyActivity.RESULT_PICK_IMAGEFILE;
+//
+//public class PostActivity extends AppCompatActivity implements View.OnClickListener {
+//    private static final int RESULT_PICK_IMAGEFILE = 1000;
+//    private ImageView imageView;
+//
+//    private FirebaseDatabase database;
+//
+//    private EditText locationText;
+//    private EditText contentText;
+//    private EditText categoryText;
+//    private DatabaseReference ref;
+//    private DatabaseReference testRef;
+//
+//public class PostActivity  extends AppCompatActivity {
 //    @Override
 //    protected void onCreate(Bundle savedInstanceState) {
 //        super.onCreate(savedInstanceState);
 //        setContentView(R.layout.post);
+//        final Button dialogBtn = (Button) findViewById(R.id.post_send_button);
+//        final TextView textView = (TextView) findViewById(R.id.post_send_button);
+//
 //
 //        locationText = findViewById(R.id.post_location_text);
 //        contentText = findViewById(R.id.post_content_text);
 //        categoryText = findViewById(R.id.post_category_text);
+//        RatingBar rate = findViewById(R.id.ratingBar);
+//        findViewById(R.id.post_back_button).setOnClickListener(this);
+//        findViewById(R.id.post_image_button).setOnClickListener(this);
+//        findViewById(R.id.post_send_button).setOnClickListener(this);
+////        rate.setOnRatingBarChangeListener(
+////                new RatingBar.OnRatingBarChangeListener(){
+////                    public void onRatingChanged(
+////                            RatingBar ratingBar,
+////                            float rating,
+////                            boolean fromUser){
+////                        Toast.makeText(
+////                                MainActivity.this,
+////                                String.format("現在の評価は%f", rating),
+////                                Toast.LENGTH_SHORT).show();
+////                    }
+////                }
+////        );
+//        ImageButton imageButton = findViewById(R.id.post_image_button);
 //
-//        findViewById(R.id.post_send_button).setOnClickListener(new View.OnClickListener() {
+////        imageButton.setOnClickListener(new View.OnClickListener() {
+////            @Override
+////            public void onClick(View v) {
+////                if (flg) {
+////                    textImageButton.setText(R.string.tapped);
+////                    flg = false;
+////                } else {
+////                    textImageButton.setText(R.string.image_button);
+////                    flg = true;
+////                }
+////            }
+////        });
+//    }
+//
+//    @Override
+//    public void onClick(View v) {
+//        PostStorage storage = new PostStorage();
+//        int i = v.getId();
+//        if (i == R.id.post_back_button) {
+//            Pin_infoActivity();
+//        } else if (i == R.id.post_image_button) {
+//            ImageVive();
+//        } else if (i == R.id.post_send_button) {
+//            storage.newCreate(categoryText.getText().toString(), contentText.getText().toString(), locationText.getText().toString());
+//        }
+//    }
+//
+//    public void Pin_infoActivity() {
+//        Intent intent = new Intent(PostActivity.this, Pin_infoActivity.class);
+//        startActivity(intent);
+//    }
+//
+//    public void ImageVive() {
+//        findViewById(R.id.post_image_button).setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View v) {
 //                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
@@ -156,8 +108,59 @@ public class PostActivity  extends AppCompatActivity {
 //            }
 //        });
 //
+//        //送信時に行う
+//        findViewById(R.id.post_send_button).setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                EditText text = (EditText)findViewById(R.id.post_content_text);
+//                if(text.getText().toString().isEmpty()) {
+//                    text.setError("文字を入力してください");
+//                } else {
+//                    dialogBtn.setOnClickListener(new View.OnClickListener() {
+//                        @Override
+//                    public void onClick(View v) {
+//                        // ダイアログクラスをインスタンス化
+//                        Post_Dialog_Confirmation dialog = new Post_Dialog_Confirmation();
+//                        // 表示  getFagmentManager()は固定、sampleは識別タグ
+//                        dialog.show(getFragmentManager(), "sample");
+//                    }
+//                });
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//                }
+//            }
+//        });
 //    }
-}
+//
+////    @Override
+////    protected void onCreate(Bundle savedInstanceState) {
+////        super.onCreate(savedInstanceState);
+////        setContentView(R.layout.post);
+////
+////        locationText = findViewById(R.id.post_location_text);
+////        contentText = findViewById(R.id.post_content_text);
+////        categoryText = findViewById(R.id.post_category_text);
+////
+////        findViewById(R.id.post_send_button).setOnClickListener(new View.OnClickListener() {
+////            @Override
+////            public void onClick(View v) {
+////                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+////                intent.addCategory(Intent.CATEGORY_OPENABLE);
+////                intent.setType("image/*");
+////                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+////                startActivityForResult(intent, RESULT_PICK_IMAGEFILE);
+////            }
+////        });
+////
+////
+////
+////
+////    }
+//}
