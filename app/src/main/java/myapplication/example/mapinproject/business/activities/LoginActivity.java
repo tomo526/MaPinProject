75d98707887c4660be2c70edf604e3cb0625af1c
nanashi <nanashi@gmail.com>
2020-01-21 14:03:37 +0900

    facebook　twitterのおおまかな追加

--- app/src/main/java/myapplication/example/mapinproject/business/activities/LoginActivity.java
+++ app/src/main/java/myapplication/example/mapinproject/business/activities/LoginActivity.java
@@ -8,13 +8,17 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.OAuthProvider;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import myapplication.example.mapinproject.R;
+import myapplication.example.mapinproject.data.entities.Test;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -22,6 +26,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     private EditText mEmailField;
     private EditText mPassField;
+    OAuthProvider.Builder provider = OAuthProvider.newBuilder("twitter.com");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +40,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         findViewById(R.id.emailSignUpButton).setOnClickListener(this);
 
         mAuth = FirebaseAuth.getInstance();
+        OAuthProvider.Builder provider = OAuthProvider.newBuilder("twitter.com");
+
     }
 
     @Override
@@ -46,8 +53,21 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             case R.id.emailSignUpButton:
                 createAccount(mEmailField.getText().toString(), mPassField.getText().toString());
                 break;
+            case R.id.Facebookbutton:
+
+                changeActivity();
+
+                break;
+            case R.id.nbutton2:
+                checklogin();
+                loginfllor();
+                loginlink();
+                changeActivity();
+
+                break;
         }
     }
+
 
     private void createAccount(String email, String password) {
         if (!validateForm()) {
@@ -110,5 +130,92 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private void changeActivity() {
         Intent intent = new Intent(LoginActivity.this, TestActivity.class);
         startActivity(intent);
+    }
+
+    private void checklogin() {
+        Task<AuthResult> pendingResultTask = mAuth.getPendingAuthResult();
+        if (pendingResultTask != null) {
+            // There's something already here! Finish the sign-in for your user.
+            pendingResultTask
+                    .addOnSuccessListener(
+                            new OnSuccessListener<AuthResult>() {
+                                @Override
+                                public void onSuccess(AuthResult authResult) {
+                                    // User is signed in.
+                                    // IdP data available in
+                                    // authResult.getAdditionalUserInfo().getProfile().
+                                    // The OAuth access token can also be retrieved:
+                                    // authResult.getCredential().getAccessToken().
+                                    // The OAuth secret can be retrieved by calling:
+                                    // authResult.getCredential().getSecret().
+                                }
+                            })
+                    .addOnFailureListener(
+                            new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    // Handle failure.
+                                }
+                            });
+        } else {
+            // There's no pending result so you need to start the sign-in flow.
+            // See below.
+        }
+
+    }
+
+    private void loginfllor() {
+        mAuth
+                .startActivityForSignInWithProvider(/* activity= */ this, provider.build())
+                .addOnSuccessListener(
+                        new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                // User is signed in.
+                                // IdP data available in
+                                // authResult.getAdditionalUserInfo().getProfile().
+                                // The OAuth access token can also be retrieved:
+                                // authResult.getCredential().getAccessToken().
+                                // The OAuth secret can be retrieved by calling:
+                                // authResult.getCredential().getSecret().
+                            }
+                        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Handle failure.
+                            }
+                        });
+    }
+
+    private void loginlink() {
+        // The user is already signed-in.
+        FirebaseUser firebaseUser = mAuth.getCurrentUser();
+
+        firebaseUser
+                .startActivityForLinkWithProvider(/* activity= */ this, provider.build())
+                .addOnSuccessListener(
+                        new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                // Twitter credential is linked to the current user.
+                                // IdP data available in
+                                // authResult.getAdditionalUserInfo().getProfile().
+                                // The OAuth access token can also be retrieved:
+                                // authResult.getCredential().getAccessToken().
+                                // The OAuth secret can be retrieved by calling:
+                                // authResult.getCredential().getSecret().
+                            }
+                        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Handle failure.
+                            }
+                        });
+
+
     }
 }
