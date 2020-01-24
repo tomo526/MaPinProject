0b963f4a4b8514a3a46a29286f76a6b0c492f037
nanashi <nanashi@gmail.com>
2020-01-24 16:33:26 +0900

    facebook　twitterの完成とユーザー登録の途中

--- app/src/main/java/myapplication/example/mapinproject/business/activities/UserRegisterActivity.java
+++ app/src/main/java/myapplication/example/mapinproject/business/activities/UserRegisterActivity.java
@@ -1,20 +1,38 @@
 package myapplication.example.mapinproject.business.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 
 import myapplication.example.mapinproject.R;
 
-public class UserRegisterActivity extends AppCompatActivity {
+public class UserRegisterActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText musernameText;
     private EditText mkomentoText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_register);
         musernameText = findViewById(R.id.usernameText3);
         mkomentoText = findViewById(R.id.komentoText6);
+        findViewById(R.id.tourokuButton).setOnClickListener(this);
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.tourokuButton:
+            //    signIn(musernameText.getText().toString(), mkomentoText.getText().toString());
+                break;
+        }
     }
 }
