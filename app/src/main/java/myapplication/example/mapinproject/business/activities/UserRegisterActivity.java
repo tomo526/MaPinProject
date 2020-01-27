b5bad30e8440ae528b927c1581474c61a9b89831
nanashi <nanashi@gmail.com>
2020-01-27 09:56:48 +0900

    facebook　twitterの完成とユーザー登録の途中(DB)

--- app/src/main/java/myapplication/example/mapinproject/business/activities/UserRegisterActivity.java
+++ app/src/main/java/myapplication/example/mapinproject/business/activities/UserRegisterActivity.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -20,22 +21,44 @@ import myapplication.example.mapinproject.R;
 public class UserRegisterActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText musernameText;
     private EditText mkomentoText;
+    private FirebaseAuth mAuth;
     final FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference ref = database.getReference("server/saving-data/fireblog");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_user_register);
         musernameText = findViewById(R.id.usernameText3);
         mkomentoText = findViewById(R.id.komentoText6);
         findViewById(R.id.tourokuButton).setOnClickListener(this);
+        mAuth = FirebaseAuth.getInstance();
     }
 
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.tourokuButton:
-            //    signIn(musernameText.getText().toString(), mkomentoText.getText().toString());
+                dbsave(musernameText.getText().toString(), mkomentoText.getText().toString());
+
                 break;
         }
     }
-}
+        private void dbsave(String email, String password) {
+//            if (!validateForm()) {
+//                return;
+//            }
+
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+//                                changeActivity();
+                            } else {
+//                                Toast.makeText(LoginActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+    }
+//}
